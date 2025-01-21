b6eaf24601ecf41c5f1ea91e99d4d769f0456816
AdityaInnovatily <aditya.v@innovatily.com>
2025-01-21 11:03:08 +0530

    updated landing page url

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -9,7 +9,7 @@ import FinalSurvey from "../components/Finalsurvey";
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/employee-engagement" element={<LandingPage />} />
+      <Route path="/" element={<LandingPage />} />
       <Route path="/employee-engagement/survey" element={<LayoutPage />}></Route>
       <Route path="employee-engagement/thank-you" element={<SuccessComponent />}></Route>
       <Route path="employee-engagement/final-survey" element={<FinalSurvey/>}></Route>
